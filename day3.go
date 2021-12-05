b1ad6c27a5bcf47760fccd6e4c96e5111f9c0f61
karlovskiy <yexella@gmail.com>
2021-12-05 03:49:38 +0300

    aoc2021 day3 part1 improvements

--- day3.go
+++ day3.go
@@ -12,13 +12,15 @@ func BinaryDiagnostic(inputFile string, debug bool) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	size := bytes.IndexByte(data, 10)
-	zeros := make([]int, size, size)
-	ones := make([]int, size, size)
-	i := 0
+	cols := bytes.IndexByte(data, 10)
+	lastRow := (len(data)+1)/(cols+1) - 1
+	zeros, ones := make([]int, cols, cols), make([]int, cols, cols)
+	gammaBits, epsilonBits := make([]byte, cols, cols), make([]byte, cols, cols)
+	i, j := 0, 0
 	for _, c := range data {
 		if c == 10 {
 			i = 0
+			j++
 			if debug {
 				log.Printf("\n")
 			}
@@ -32,32 +34,26 @@ func BinaryDiagnostic(inputFile string, debug bool) (int64, error) {
 		} else if c == 49 {
 			ones[i]++
 		}
+		if j == lastRow {
+			if zeros[i] > ones[i] {
+				gammaBits[i] = 48
+				epsilonBits[i] = 49
+			} else if ones[i] > zeros[i] {
+				gammaBits[i] = 49
+				epsilonBits[i] = 48
+			}
+		}
 		i++
 	}
 	if debug {
-		log.Printf("zeros: %v", zeros)
-		log.Printf("ones:  %v", ones)
+		log.Printf("zeros: %v, ones: %v", zeros, ones)
+		log.Printf("g: %s, e: %s", gammaBits, epsilonBits)
 	}
-	g := make([]byte, size, size)
-	e := make([]byte, size, size)
-	for i := 0; i < size; i++ {
-		if zeros[i] > ones[i] {
-			g[i] = 48
-			e[i] = 49
-		} else if ones[i] > zeros[i] {
-			g[i] = 49
-			e[i] = 48
-		}
-	}
-	if debug {
-		log.Printf("g: %s", g)
-		log.Printf("e: %s", e)
-	}
-	gammaRate, err := strconv.ParseInt(string(g), 2, 64)
+	gammaRate, err := strconv.ParseInt(string(gammaBits), 2, 64)
 	if err != nil {
 		return 0, err
 	}
-	epsilonRate, err := strconv.ParseInt(string(e), 2, 64)
+	epsilonRate, err := strconv.ParseInt(string(epsilonBits), 2, 64)
 	if err != nil {
 		return 0, err
 	}
